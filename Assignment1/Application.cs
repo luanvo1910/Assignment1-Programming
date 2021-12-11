0ac8a5cac1a3b4915e9a15962f54e6669331253d
luan <oga.tatsumi1230410@gmail.com>
2021-12-11 14:22:31 +0700

    Add project files.

---
+++ Assignment1/Application.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Application
+    {
+        public void Direction()
+        {
+            Book Book = new Book();
+            int choice, removeId, editId, confirm;
+            string name, author, category, publicDate;
+            do
+            {
+                Console.Clear();
+                Book.PrintBooks();
+                Console.Write("What will you do?\n1. Search book\n2. Add a new book\n3. Remove book\n4. Edit book\n5. End\nChoice: ");
+                choice = int.Parse(Console.ReadLine());
+                
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter name of Book or Author to search: ");
+                        string Search = Console.ReadLine();
+
+                        Book.SearchBook(Search);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Enter Name: ");
+                        name = Console.ReadLine();
+                        Console.Write("Enter Author: ");
+                        author = Console.ReadLine();
+                        Console.Write("Enter Category: ");
+                        category = Console.ReadLine();
+                        Console.Write("publication Date: ");
+                        publicDate = Console.ReadLine();
+                        Book.AddBook(name, author, category, publicDate);
+                        break;
+                    case 3:
+                        Console.Write("Enter book's Id to remove book: ");
+                        removeId = int.Parse(Console.ReadLine());
+                        Book.PrintBook(removeId);
+                        Console.Write("Please confirm to delete this book!\n1. Yes\n2. No\nChoice: ");
+                        confirm = int.Parse(Console.ReadLine());
+                        if (confirm == 1)
+                        {
+                            Book.Remove(removeId);
+                        }    
+                        break;
+                    case 4:
+                        Console.Write("Enter book's Id to edit book: ");
+                        editId = int.Parse(Console.ReadLine());
+                        Book.PrintBook(editId);
+                        Console.Write("Please confirm to edit this book!\n1. Yes\n2. No\nChoice: ");
+                        confirm = int.Parse(Console.ReadLine());
+                        if (confirm == 1)
+                        {
+                            Console.Write("Enter Name: ");
+                            name = Console.ReadLine();
+                            Console.Write("Enter Author: ");
+                            author = Console.ReadLine();
+                            Console.Write("Enter Category: ");
+                            category = Console.ReadLine();
+                            Console.Write("publication Date: ");
+                            publicDate = Console.ReadLine();
+                            Book.Edit(editId, name, author, category, publicDate);
+                        }
+                        break;
+                }
+            } while (choice != 5);
+        }
+    }
+}
---
+++ Assignment1/Book.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Book
+    {
+        public int Id {get; set;}
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string Category { get; set; }
+
+        public string PublicationDate { get; set; }
+
+        public List<Book> Books = new List<Book>();
+
+        public void PrintBooks()
+        {
+            Console.WriteLine("Id\tName\t\tAuthor\t\t\tCategory\tPublication Date");
+            foreach (var item in Books)
+            {
+                Console.WriteLine($"{item.Id}\t{item.Name}\t{item.Author}\t\t{item.Category}\t\t{item.PublicationDate}");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void PrintBook(int Id)
+        {
+            Console.WriteLine("Id\tName\t\tAuthor\t\t\tCategory\tPublication Date");
+            foreach (var item in Books)
+            {
+                if (item.Id == Id)
+                {
+                    Console.WriteLine($"{item.Id}\t{item.Name}\t{item.Author}\t\t{item.Category}\t\t{item.PublicationDate}");
+                }         
+            }
+        }
+
+        public void AddBook(string Name, string Author, string Cate, string publibDate)
+        {
+            Books.Add(new Book() { Id = Books.Count + 1, Name = Name, Author = Author, Category = Cate, PublicationDate = publibDate });
+        }
+
+        public void SearchBook(string searchString)
+        {
+            foreach(var item in Books)
+            {
+                if (item.Name == searchString || item.Author == searchString)
+                {
+                    Console.WriteLine($"ID: {item.Id}\nName: {item.Name}\nAuthor: {item.Author}\nCategory: {item.Category}\nPublication Date: {item.PublicationDate}");
+                }    
+            }    
+        }
+
+        public void Remove(int RemoveId)
+        {
+            var bookToRemove = Books.Single(b => b.Id == RemoveId);
+            Books.Remove(bookToRemove);
+
+        }
+
+        public void Edit(int EditId, string Name, string Author, string Cate, string publibDate)
+        {
+            var bookToRemove = Books.Single(b => b.Id == EditId);
+            Books.Remove(bookToRemove);
+
+            Books.Add(new Book() { Id = EditId, Name = Name, Author = Author, Category = Cate, PublicationDate = publibDate });
+        }
+
+    }
+}
